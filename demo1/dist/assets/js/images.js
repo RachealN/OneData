dbef6e8a28e5253cbd2cd1a9aa539fe69acba3a3
RachealN <rechealmatsikoit@gmail.com>
2024-08-28 17:18:34 +0300

    separate images and videos

--- demo1/dist/assets/js/images.js
+++ demo1/dist/assets/js/images.js
@@ -1,32 +1,39 @@
 $(".slider").slick({
-    dots: true,
-    infinite: false,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
+  dots: true,
+  infinite: false,
+  slidesToShow: 1, 
+  slidesToScroll: 1, 
+  rows: 2, 
+  slidesToShow: 3, 
+  slidesToScroll: 3, 
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows: 2
       },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows: 2
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        rows: 2
       },
-    ],
-  });
+    },
+  ],
+});
+
 
 var modal = document.getElementById("imageModal");
 var modalImg = document.getElementById("expandedImg");
@@ -43,23 +50,24 @@ $(".slider .overlay").on("click", function() {
 });
 
 $(document).ready(function() {
-    $(".video-thumbnail").on("click", function() {
-        var videoUrl = $(this).data("video-url");
-        $("#videoFrame").attr("src", videoUrl);
-        $("#videoModal").fadeIn(); 
-    });
+  $(".video-thumbnail").on("click", function() {
+      var videoUrl = $(this).data("video-url");
+      $("#videoSource").attr("src", videoUrl);
+      $("#videoPlayer")[0].load();
+      $("#videoModal").fadeIn(); 
+  });
 
-    $(document).on("click", ".modal .close", function() {
-        $(this).closest('.modal').fadeOut(); 
-        $("#videoFrame").attr("src", ""); 
-    });
+  $(document).on("click", ".modal .close", function() {
+      $(this).closest('.modal').fadeOut(); 
+      $("#videoPlayer")[0].pause();
+      $("#videoSource").attr("src", "");
+  });
 
-    $(window).on("click", function(event) {
-        if ($(event.target).hasClass('modal')) {
-            $(event.target).fadeOut();
-            $("#videoFrame").attr("src", ""); 
-        }
-    });
+  $(window).on("click", function(event) {
+      if ($(event.target).hasClass('modal')) {
+          $(event.target).fadeOut();
+          $("#videoPlayer")[0].pause();
+          $("#videoSource").attr("src", "");
+      }
+  });
 });
-
-
